a1eb9f40070e158cd4346a75a983b91b1968aa13
nsuubhan62 <ranasubhansoj@gmail.com>
2024-07-08 19:21:23 +0500

    vors e

--- App.js
+++ App.js
@@ -10,6 +10,17 @@ const FacebookUser = require("./models/facebookUserSchema"); // Assuming this is
 dotenv.config();
 connectDB();
 const app = express();
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://onestopstore1.netlify.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   cors({
     origin: ["https://onestopstore1.netlify.app"], // allow requests from this domain
