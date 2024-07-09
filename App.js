0248013b539dd01d588a9cd8ec12d366134a2ee9
nsuubhan62 <ranasubhansoj@gmail.com>
2024-07-09 15:51:39 +0500

    vercel

--- App.js
+++ App.js
@@ -114,6 +114,7 @@ app.use((req, res) => {
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
 
 // app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 // app.use(passport.initialize());
