584a086c26d6661c9aadcea304c285a2202a37b4
nsuubhan62 <ranasubhansoj@gmail.com>
2024-07-09 15:15:57 +0500

    vercel commit

--- App.js
+++ App.js
@@ -92,20 +92,20 @@ app.get("/api/test", (req, res) => {
 });
 
 // Handle Vercel deployment
-module.exports = (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+// module.exports = (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // if (req.method === "OPTIONS") {
-  //   return res.status(200).end();
-  // }
+//   // if (req.method === "OPTIONS") {
+//   //   return res.status(200).end();
+//   // }
 
-  // Your logic here
-};
+//   // Your logic here
+// };
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
