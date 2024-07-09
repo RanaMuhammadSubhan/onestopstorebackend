add3497e75040dd800f1f319724653074d1deaba
nsuubhan62 <ranasubhansoj@gmail.com>
2024-07-09 15:30:32 +0500

    vercel

--- App.js
+++ App.js
@@ -84,28 +84,29 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-const authRoutes = require("./routes/facebookroute");
-app.use("/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.get("/api/test", (req, res) => {
-  res.send("API is working");
-});
 
 // Handle Vercel deployment
-// module.exports = (req, res) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-//   // if (req.method === "OPTIONS") {
-//   //   return res.status(200).end();
-//   // }
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
-//   // Your logic here
-// };
+  const authRoutes = require("./routes/facebookroute");
+  app.use("/auth", authRoutes);
+  app.use("/api/users", userRoutes);
+  app.get("/api/test", (req, res) => {
+    res.send("API is working");
+  });
+  // Your logic here
+  res.status(200).json({ message: "Hello from Vercel" });
+};
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
